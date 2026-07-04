cb7e4fbd6c1ef33edc17d9cebe3cda3ede251342
YukiYonekura <yoneyuu321@gmail.com>
2026-07-04 15:44:44 +0900

    /api/likesエンドポイント作成(コントローラ層)

---
+++ src/main/java/com/example/backend/controller/LikeController.java
@@ -0,0 +1,28 @@
+package com.example.backend.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.backend.dto.IdNameResponse;
+import com.example.backend.service.LikeService;
+
+@RestController
+@RequestMapping("/api/likes")
+public class LikeController {
+
+    private final LikeService likeService;
+
+    public LikeController(LikeService likeService) {
+        this.likeService = likeService;
+    }
+
+    // GET /api/likes
+    @GetMapping
+    public ResponseEntity<List<IdNameResponse>> index() {
+        return ResponseEntity.ok(likeService.index());
+    }
+}
